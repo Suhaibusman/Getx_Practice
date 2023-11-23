f6d0a8490599ac39bd90113c9a02b70b010f3583
Suhaibusman <suhaibusman54@gmail.com>
2023-11-23 23:15:29 +0500

    getx practice

--- lib/ui/counter_view.dart
+++ lib/ui/counter_view.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:getx_practice/controller/counter_controller.dart';
+import 'package:getx_practice/ui/home_view.dart';
 
 class CounterView extends StatelessWidget {
   const CounterView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    CounterController counterController = CounterController();
+      CounterController counterController = Get.put(CounterController());
+    
+    // CounterController counterController = CounterController();
     return  Scaffold(
       appBar: AppBar(title: const Text("Counter View"),),
       body: Center(
         child: Column(
           children: [
-          Obx(()=>Text(counterController.count.value.toString()))
+          Obx(()=>Text(counterController.count.value.toString())),
+          ElevatedButton(onPressed: (){
+            Get.to(const HomeView());
+          }, child: const Text("Navigate to Home"))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){
-        counterController.incrementCount();
+        counterController.incrementCount(); 
       }, child: const Icon(Icons.add),),
     );
   }
--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_practice/controller/counter_controller.dart';
+import 'package:getx_practice/ui/counter_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    CounterController counterController = Get.find<CounterController>();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home View"),),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+        Obx(() =>   Text("Counter Value ${counterController.count}"),),
+            ElevatedButton(onPressed: (){
+            Get.to(const CounterView());
+          }, child: const Text("Navigate to CounterScreen")),
+          
+         
+        ],
+      ),
+    ),
+         floatingActionButton: FloatingActionButton(onPressed: (){
+        counterController.incrementCount(); 
+      }, child: const Icon(Icons.add),),
+    );
   }
 }
